cfb85f9e26d717a3b54e461043f312cd1f7cc1b9
liuying1129 <liuying1129@163.com>
2017-05-25 22:55:39 +0800

    目录初始化

---
+++ Sur/UfrmMain.pas
@@ -0,0 +1,546 @@
+unit UfrmMain;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Classes, Controls, Forms,
+  LYTray, Menus, StdCtrls, Buttons, ADODB,
+  ActnList, AppEvnts, ComCtrls, ToolWin, ExtCtrls,
+  registry,inifiles,Dialogs,
+  StrUtils, DB,ComObj,Variants, ScktComp;
+
+type 
+  client_record=record 
+         CHandle: integer; //客户端套接字句柄 
+         CSocket:TCustomWinSocket; //客户端套接字 
+         CName:string; //客户端计算机名称 
+         CAddress:string; //客户端计算机IP地址 
+         CUsed: boolean; //客户端联机标志 
+end; 
+  
+type
+  TfrmMain = class(TForm)
+    LYTray1: TLYTray;
+    PopupMenu1: TPopupMenu;
+    N1: TMenuItem;
+    N2: TMenuItem;
+    N3: TMenuItem;
+    ADOConnection1: TADOConnection;
+    ApplicationEvents1: TApplicationEvents;
+    CoolBar1: TCoolBar;
+    ToolBar1: TToolBar;
+    ToolButton3: TToolButton;
+    ToolButton4: TToolButton;
+    ToolButton7: TToolButton;
+    ToolButton8: TToolButton;
+    ActionList1: TActionList;
+    editpass: TAction;
+    about: TAction;
+    stop: TAction;
+    ToolButton2: TToolButton;
+    Memo1: TMemo;
+    BitBtn1: TBitBtn;
+    BitBtn2: TBitBtn;
+    Button1: TButton;
+    ToolButton5: TToolButton;
+    ToolButton9: TToolButton;
+    OpenDialog1: TOpenDialog;
+    ServerSocket1: TServerSocket;
+    procedure N3Click(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+    //增加病人信息表中记录,返回该记录的唯一编号作为检验结果表的外键
+    procedure FormClose(Sender: TObject; var Action: TCloseAction);
+    procedure N1Click(Sender: TObject);
+    procedure ApplicationEvents1Activate(Sender: TObject);
+    procedure ToolButton7Click(Sender: TObject);
+    procedure ToolButton2Click(Sender: TObject);
+    procedure BitBtn2Click(Sender: TObject);
+    procedure BitBtn1Click(Sender: TObject);
+    procedure Button1Click(Sender: TObject);
+    procedure ToolButton5Click(Sender: TObject);
+    procedure ServerSocket1ClientRead(Sender: TObject;
+      Socket: TCustomWinSocket);
+    procedure ServerSocket1ClientConnect(Sender: TObject;
+      Socket: TCustomWinSocket);
+    procedure ServerSocket1ClientDisconnect(Sender: TObject;
+      Socket: TCustomWinSocket);
+    procedure ServerSocket1ClientError(Sender: TObject;
+      Socket: TCustomWinSocket; ErrorEvent: TErrorEvent;
+      var ErrorCode: Integer);
+    procedure ServerSocket1GetSocket(Sender: TObject; Socket: Integer;
+      var ClientSocket: TServerClientWinSocket);
+    procedure ServerSocket1Listen(Sender: TObject;
+      Socket: TCustomWinSocket);
+  private
+    { Private declarations }
+    procedure WMSyscommand(var message:TWMMouse);message WM_SYSCOMMAND;
+    procedure UpdateConfig;{配置文件生效}
+    function LoadInputPassDll:boolean;
+    function MakeDBConn:boolean;
+    function DIFF_decode(const Value:string):string;
+    function GetSpecNo(const Value:string):string; //取得联机号
+  public
+    { Public declarations }
+    Session: array[0..10] of client_record; //客户端连接数组
+    Sessions: integer; //客户端连接数
+  end;
+
+var
+  frmMain: TfrmMain;
+
+implementation
+
+uses ucommfunction;
+
+const
+  CR=#$D+#$A;
+  STX=#$2;ETX=#$3;ACK=#$6;NAK=#$15;
+  sCryptSeed='lc';//加解密种子
+  //SEPARATOR=#$1C;
+  sCONNECTDEVELOP='错误!请与开发商联系!' ;
+  IniSection='Setup';
+
+var
+  ConnectString:string;
+  GroupName:string;//
+  SpecType:string ;//
+  SpecStatus:string ;//
+  CombinID:string;//
+  LisFormCaption:string;//
+  QuaContSpecNoG:string;
+  QuaContSpecNo:string;
+  QuaContSpecNoD:string;
+  EquipChar:string;
+
+  RFM:STRING;       //返回数据
+  hnd:integer;
+  bRegister:boolean;
+
+{$R *.dfm}
+
+function ifRegister:boolean;
+var
+  HDSn,RegisterNum,EnHDSn:string;
+  configini:tinifile;
+  pEnHDSn:Pchar;
+begin
+  result:=false;
+  
+  HDSn:=GetHDSn('C:\')+'-'+GetHDSn('D:\')+'-'+ChangeFileExt(ExtractFileName(Application.ExeName),'');
+
+  CONFIGINI:=TINIFILE.Create(ChangeFileExt(Application.ExeName,'.ini'));
+  RegisterNum:=CONFIGINI.ReadString(IniSection,'RegisterNum','');
+  CONFIGINI.Free;
+  pEnHDSn:=EnCryptStr(Pchar(HDSn),sCryptSeed);
+  EnHDSn:=StrPas(pEnHDSn);
+
+  if Uppercase(EnHDSn)=Uppercase(RegisterNum) then result:=true;
+
+  if not result then messagedlg('对不起,您没有注册或注册码错误,请注册!',mtinformation,[mbok],0);
+end;
+
+function GetConnectString:string;
+var
+  Ini:tinifile;
+  userid, password, datasource, initialcatalog: string;
+  ifIntegrated:boolean;//是否集成登录模式
+
+  pInStr,pDeStr:Pchar;
+  i:integer;
+begin
+  result:='';
+  
+  Ini := tinifile.Create(ChangeFileExt(Application.ExeName,'.INI'));
+  datasource := Ini.ReadString('连接数据库', '服务器', '');
+  initialcatalog := Ini.ReadString('连接数据库', '数据库', '');
+  ifIntegrated:=ini.ReadBool('连接数据库','集成登录模式',false);
+  userid := Ini.ReadString('连接数据库', '用户', '');
+  password := Ini.ReadString('连接数据库', '口令', '107DFC967CDCFAAF');
+  Ini.Free;
+  //======解密password
+  pInStr:=pchar(password);
+  pDeStr:=DeCryptStr(pInStr,sCryptSeed);
+  setlength(password,length(pDeStr));
+  for i :=1  to length(pDeStr) do password[i]:=pDeStr[i-1];
+  //==========
+
+  result := result + 'user id=' + UserID + ';';
+  result := result + 'password=' + Password + ';';
+  result := result + 'data source=' + datasource + ';';
+  result := result + 'Initial Catalog=' + initialcatalog + ';';
+  result := result + 'provider=' + 'SQLOLEDB.1' + ';';
+  if ifIntegrated then
+    result := result + 'Integrated Security=SSPI;';
+end;
+
+procedure TfrmMain.FormCreate(Sender: TObject);
+var
+  ctext        :string;
+  reg          :tregistry;
+begin
+  rfm:='';
+
+  ConnectString:=GetConnectString;
+  
+  UpdateConfig;
+  if ifRegister then bRegister:=true else bRegister:=false;  
+
+  lytray1.Hint:='数据接收服务'+ExtractFileName(Application.ExeName);
+
+//=============================初始化密码=====================================//
+    reg:=tregistry.Create;
+    reg.RootKey:=HKEY_CURRENT_USER;
+    reg.OpenKey('\sunyear',true);
+    ctext:=reg.ReadString('pass');
+    if ctext='' then
+    begin
+        reg:=tregistry.Create;
+        reg.RootKey:=HKEY_CURRENT_USER;
+        reg.OpenKey('\sunyear',true);
+        reg.WriteString('pass','JIHONM{');
+        //MessageBox(application.Handle,pchar('感谢您使用智能监控系统，'+chr(13)+'请记住初始化密码：'+'lc'),
+        //            '系统提示',MB_OK+MB_ICONinformation);     //WARNING
+    end;
+    reg.CloseKey;
+    reg.Free;
+//============================================================================//
+end;
+
+procedure TfrmMain.FormClose(Sender: TObject; var Action: TCloseAction);
+begin
+    if LoadInputPassDll then begin ServerSocket1.Close;action:=cafree;end else action:=caNone;
+end;
+
+procedure TfrmMain.N3Click(Sender: TObject);
+begin
+    if not LoadInputPassDll then exit;
+    application.Terminate;
+end;
+
+procedure TfrmMain.N1Click(Sender: TObject);
+begin
+  show;
+end;
+
+procedure TfrmMain.ApplicationEvents1Activate(Sender: TObject);
+begin
+  hide;
+end;
+
+procedure TfrmMain.WMSyscommand(var message: TWMMouse);
+begin
+  inherited;
+  if message.Keys=SC_MINIMIZE then hide;
+  message.Result:=-1;
+end;
+
+procedure TfrmMain.ToolButton7Click(Sender: TObject);
+begin
+  if MakeDBConn then ConnectString:=GetConnectString;
+end;
+
+procedure TfrmMain.UpdateConfig;
+var
+  INI:tinifile;
+  autorun:boolean;
+  ServerPort:integer;
+begin
+  ini:=TINIFILE.Create(ChangeFileExt(Application.ExeName,'.ini'));
+
+  ServerPort:=ini.ReadInteger(IniSection,'服务器端口',8080);
+
+  autorun:=ini.readBool(IniSection,'开机自动运行',false);
+
+  GroupName:=trim(ini.ReadString(IniSection,'组别',''));
+  EquipChar:=trim(uppercase(ini.ReadString(IniSection,'仪器字母','')));//读出来是大写就万无一失了
+  SpecType:=ini.ReadString(IniSection,'默认样本类型','');
+  SpecStatus:=ini.ReadString(IniSection,'默认样本状态','');
+  CombinID:=ini.ReadString(IniSection,'组合项目代码','');
+
+  LisFormCaption:=ini.ReadString(IniSection,'检验系统窗体标题','');
+
+  QuaContSpecNoG:=ini.ReadString(IniSection,'高值质控联机号','9999');
+  QuaContSpecNo:=ini.ReadString(IniSection,'常值质控联机号','9998');
+  QuaContSpecNoD:=ini.ReadString(IniSection,'低值质控联机号','9997');
+
+  ini.Free;
+
+  OperateLinkFile(application.ExeName,'\'+ChangeFileExt(ExtractFileName(Application.ExeName),'.lnk'),15,autorun);
+
+  ServerSocket1.Close;
+  ServerSocket1.Port:=ServerPort;
+  try
+    ServerSocket1.Open;
+  except
+    showmessage('端口打开失败!');
+  end;
+end;
+
+function TfrmMain.LoadInputPassDll: boolean;
+TYPE
+    TDLLFUNC=FUNCTION:boolean;
+VAR
+    HLIB:THANDLE;
+    DLLFUNC:TDLLFUNC;
+    PassFlag:boolean;
+begin
+    result:=false;
+    HLIB:=LOADLIBRARY('OnOffLogin.dll');
+    IF HLIB=0 THEN BEGIN SHOWMESSAGE(sCONNECTDEVELOP);EXIT; END;
+    DLLFUNC:=TDLLFUNC(GETPROCADDRESS(HLIB,'showfrmonofflogin'));
+    IF @DLLFUNC=NIL THEN BEGIN SHOWMESSAGE(sCONNECTDEVELOP);EXIT; END;
+    PassFlag:=DLLFUNC;
+    FREELIBRARY(HLIB);
+    result:=passflag;
+end;
+
+function TfrmMain.GetSpecNo(const Value:string):string; //取得联机号
+VAR
+  i:integer;
+  s:string;
+  ls2:tstrings;
+begin
+  i:=pos('OBR|',uppercase(Value));
+  if i<=0 then
+  begin
+    result:=formatdatetime('nnss',now);
+    exit;
+  end;
+  
+  s:=Value;
+  delete(s,1,i+3);
+
+  ls2:=StrToList(s,'|');
+  if ls2.Count<3 then
+  begin
+    result:=formatdatetime('nnss',now);
+    ls2.Free;
+    exit;
+  end;
+  
+  result:=ls2[2];
+  ls2.Free;
+  
+  //i:=pos('|',s);
+  //delete(s,1,i);
+  //i:=pos('|',s);
+  //delete(s,1,i);
+  //i:=pos('|',s);
+  //  result:=trim(COPY(s,1,i-1));
+    result:='0000'+result;
+    result:=rightstr(result,4);
+end;
+
+function TfrmMain.DIFF_decode(const Value:string):string;
+begin
+  result:=stringreplace(Value,'#',' ',[rfReplaceAll,rfIgnoreCase]);
+  result:=trim(result);
+end;
+
+function TfrmMain.MakeDBConn:boolean;
+var
+  newconnstr,ss: string;
+  Label labReadIni;
+begin
+  result:=false;
+
+  labReadIni:
+  newconnstr := GetConnectString;
+  
+  try
+    ADOConnection1.Connected := false;
+    ADOConnection1.ConnectionString := newconnstr;
+    ADOConnection1.Connected := true;
+    result:=true;
+  except
+  end;
+  if not result then
+  begin
+    ss:='服务器'+#2+'Edit'+#2+#2+'0'+#2+#2+#3+
+        '数据库'+#2+'Edit'+#2+#2+'0'+#2+#2+#3+
+        '集成登录模式'+#2+'CheckListBox'+#2+#2+'0'+#2+#2+#3+
+        '用户'+#2+'Edit'+#2+#2+'0'+#2+#2+#3+
+        '口令'+#2+'Edit'+#2+#2+'0'+#2+#2+'1';
+    if ShowOptionForm('连接数据库','连接数据库',Pchar(ss),Pchar(ChangeFileExt(Application.ExeName,'.ini'))) then
+      goto labReadIni else application.Terminate;
+  end;
+end;
+
+procedure TfrmMain.ToolButton2Click(Sender: TObject);
+var
+  ss:string;
+begin
+  if LoadInputPassDll then
+  begin
+    ss:='服务器端口'+#2+'Edit'+#2+#2+'1'+#2+#2+#3+
+      '组别'+#2+'Edit'+#2+#2+'1'+#2+#2+#3+
+      '仪器字母'+#2+'Edit'+#2+#2+'1'+#2+#2+#3+
+      '检验系统窗体标题'+#2+'Edit'+#2+#2+'1'+#2+#2+#3+
+      '默认样本类型'+#2+'Edit'+#2+#2+'1'+#2+#2+#3+
+      '默认样本状态'+#2+'Edit'+#2+#2+'1'+#2+#2+#3+
+      '组合项目代码'+#2+'Edit'+#2+#2+'1'+#2+#2+#3+
+      '开机自动运行'+#2+'CheckListBox'+#2+#2+'1'+#2+#2+#3+
+      '高值质控联机号'+#2+'Edit'+#2+#2+'2'+#2+#2+#3+
+      '常值质控联机号'+#2+'Edit'+#2+#2+'2'+#2+#2+#3+
+      '低值质控联机号'+#2+'Edit'+#2+#2+'2'+#2+#2;
+
+  if ShowOptionForm('',Pchar(IniSection),Pchar(ss),Pchar(ChangeFileExt(Application.ExeName,'.ini'))) then
+	  UpdateConfig;
+  end;
+end;
+
+procedure TfrmMain.BitBtn2Click(Sender: TObject);
+begin
+  Memo1.Lines.Clear;
+end;
+
+procedure TfrmMain.BitBtn1Click(Sender: TObject);
+begin
+  memo1.Lines.SaveToFile('c:\comm.txt');
+  showmessage('保存成功!');
+end;
+
+procedure TfrmMain.Button1Click(Sender: TObject);
+var
+  ls:Tstrings;
+begin
+  OpenDialog1.DefaultExt := '.txt';
+  OpenDialog1.Filter := 'txt (*.txt)|*.txt';
+  if not OpenDialog1.Execute then exit;
+  ls:=Tstringlist.Create;
+  ls.LoadFromFile(OpenDialog1.FileName);
+  ServerSocket1ClientRead(nil,nil);
+  ls.Free;
+end;
+
+procedure TfrmMain.ToolButton5Click(Sender: TObject);
+var
+  ss:string;
+begin
+  ss:='RegisterNum'+#2+'Edit'+#2+#2+'0'+#2+'将该窗体标题栏上的字符串发给开发者,以获取注册码'+#2;
+  if bRegister then exit;
+  if ShowOptionForm(Pchar('注册:'+GetHDSn('C:\')+'-'+GetHDSn('D:\')+'-'+ChangeFileExt(ExtractFileName(Application.ExeName),'')),Pchar(IniSection),Pchar(ss),Pchar(ChangeFileExt(Application.ExeName,'.ini'))) then
+    if ifRegister then bRegister:=true else bRegister:=false;
+end;
+
+procedure TfrmMain.ServerSocket1ClientRead(Sender: TObject;
+  Socket: TCustomWinSocket);
+var
+  SpecNo:string;
+  rfm2:string;
+  sValue:string;
+  FInts:OleVariant;
+  ReceiveItemInfo:OleVariant;
+  i,j:integer;
+  Str:string;
+  SBPos,EBPos:integer;
+  ls,ls2:tstrings;
+  DtlStr:string;
+begin
+  Str:=Socket.ReceiveText;
+  if length(memo1.Lines.Text)>=60000 then memo1.Lines.Clear;//memo只能接受64K个字符
+  memo1.Lines.Add(Str);
+
+  rfm:=rfm+Str;
+  
+  SBPos:=pos(#$0B,rfm);
+  if SBPos<=0 then exit;
+  delete(rfm,1,SBPos-1);//保持头是第一个字符
+
+  EBPos:=pos(#$1C#$0D,rfm);
+  while EBPos>0 do
+  begin
+    rfm2:=copy(rfm,1,EBPos+1);//1个标本结果
+    delete(rfm,1,EBPos+1);
+
+    SpecNo:=GetSpecNo(rfm2);
+
+    ls:=TStringList.Create;
+    ExtractStrings([#$D],[],Pchar(rfm2),ls);
+
+    ReceiveItemInfo:=VarArrayCreate([0,ls.Count-1],varVariant);
+
+    for  i:=0  to ls.Count-1 do
+    begin
+      DtlStr:='';
+      sValue:='';
+      if uppercase(copy(trim(ls[i]),1,4))='OBX|' then
+      begin
+        ls2:=StrToList(ls[i],'|');
+        if ls2.Count<6 then begin ls2.Free;continue; end;
+        DtlStr:=ls2[4];
+        sValue:=ls2[5];
+        ls2.Free;
+      end;
+      ReceiveItemInfo[i]:=VarArrayof([DtlStr,sValue,'','']);
+
+      //处理重做结果Start
+      for  j:=0  to i-1 do
+      begin
+        if (DtlStr<>'')and(ReceiveItemInfo[j][0]=DtlStr) then ReceiveItemInfo[j]:=VarArrayof(['','','','']);
+      end;
+      //处理重做结果End
+    end;
+    ls.Free;
+
+    if bRegister then
+    begin
+      FInts :=CreateOleObject('Data2LisSvr.Data2Lis');
+      FInts.fData2Lis(ReceiveItemInfo,(SpecNo),'',
+        (GroupName),(SpecType),(SpecStatus),(EquipChar),
+        (CombinID),'',(LisFormCaption),(ConnectString),
+        (QuaContSpecNoG),(QuaContSpecNo),(QuaContSpecNoD),'',
+        true,true,'常规');
+      if not VarIsEmpty(FInts) then FInts:= unAssigned;
+    end;
+
+    EBPos:=pos(#$1C#$0D,rfm);
+  end;
+
+end;
+
+procedure TfrmMain.ServerSocket1ClientConnect(Sender: TObject;
+  Socket: TCustomWinSocket);
+begin
+  Memo1.Lines.Add('客户端'+Socket.RemoteHost+'('+Socket.RemoteAddress+')已经连接');
+end;
+
+procedure TfrmMain.ServerSocket1ClientDisconnect(Sender: TObject;
+  Socket: TCustomWinSocket);
+begin
+  Memo1.Lines.Add('客户端'+Socket.RemoteHost+'('+Socket.RemoteAddress+')已经断开');
+end;
+
+procedure TfrmMain.ServerSocket1ClientError(Sender: TObject;
+  Socket: TCustomWinSocket; ErrorEvent: TErrorEvent;
+  var ErrorCode: Integer);
+begin
+  Memo1.Lines.Add('客户端'+Socket.RemoteHost+'('+Socket.RemoteAddress+')发生错误');
+  ErrorCode := 0;
+end;
+
+procedure TfrmMain.ServerSocket1GetSocket(Sender: TObject; Socket: Integer;
+  var ClientSocket: TServerClientWinSocket);
+begin
+  //Memo1.Lines.Add('客户端正在连接...');
+end;
+
+procedure TfrmMain.ServerSocket1Listen(Sender: TObject;
+  Socket: TCustomWinSocket);
+begin
+  //Memo1.Lines.Add('等待客户端连接...');
+end;
+
+initialization
+    hnd := CreateMutex(nil, True, Pchar(ExtractFileName(Application.ExeName)));
+    if GetLastError = ERROR_ALREADY_EXISTS then
+    begin
+        MessageBox(application.Handle,pchar('该程序已在运行中！'),
+                    '系统提示',MB_OK+MB_ICONinformation);   
+        Halt;
+    end;
+
+finalization
+    if hnd <> 0 then CloseHandle(hnd);
+
+end.
